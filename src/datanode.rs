ac5292a2b00d561e523eda0d9ef3708eadc4545a
liamd101 <liamdodds101@gmail.com>
2023-11-06 20:04:06 -0500

    Basic DataNode struct

---
+++ src/datanode.rs
@@ -0,0 +1,24 @@
+struct Block {
+    id: usize,
+    data: Vec<u8>,
+}
+
+struct DataNode {
+    blocks: HashMap<usize, Block>,
+}
+
+impl DataNode {
+    fn new() -> Self {
+        DataNode {
+            blocks: HashMap::new(),
+        }
+    }
+
+    fn write_block(&mut self, block: Block) {
+        self.blocks.insert(block.id, block);
+    }
+
+    fn read_block(&self, id: usize) -> Option<&Block> {
+        self.blocks.get(&id)
+    }
+}
