8ddb4ac891420bbb5903c59ab0600111f7a9513f
liamd101 <liamdodds101@gmail.com>
2023-11-18 17:33:12 -0500

    added testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use clap::{Parser, Subcommand};
 pub mod block;
 pub mod datanode;
-use clap::{Parser, Subcommand};
+use datanode::DataNodeServer;
+use tokio::io::AsyncReadExt;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about= None)]
@@ -11,15 +13,37 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    Datanode {},
+    Datanode { port: String },
     Namenode {},
 }
 
-fn main() {
-    let args = Args::parse();
+async fn test(port: String) -> Result<(), Box<dyn std::error::Error>> {
+    let server = DataNodeServer::new(port);
+    let listener = server.create_listener().await?;
+    server.bind_port().await?;
 
+    match listener.accept().await {
+        Ok((socket, _addr)) => {
+            let mut buf = [0; 1024];
+            let mut stream = socket;
+            let n = stream.read(&mut buf).await?;
+            if n > 0 {
+                println!("Received: {:?}", &buf[..n]);
+            }
+        }
+        Err(e) => println!("couldn't get client: {:?}", e),
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
     match args.command {
-        Command::Datanode {} => todo!(),
+        Command::Datanode { port } => {
+            let _ = test(port).await;
+        }
         Command::Namenode {} => todo!(),
     }
 }
