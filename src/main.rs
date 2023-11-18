636071157f52a591cd72faa74aa06b0753e7a52f
liamd101 <liamdodds101@gmail.com>
2023-11-18 12:29:10 -0500

    added clap for CLI interaction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+pub mod block;
+pub mod datanode;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about= None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Datanode {},
+    Namenode {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Datanode {} => todo!(),
+        Command::Namenode {} => todo!(),
+    }
 }
