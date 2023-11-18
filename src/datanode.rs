e83f13f9e08e2aaa9b441fa9648f1dfe211996b5
liamd101 <liamdodds101@gmail.com>
2023-11-18 17:25:25 -0500

    moved storage into its own module

--- src/datanode.rs
+++ src/datanode.rs
@@ -1,44 +1,80 @@
 #![allow(dead_code)]
-use crate::block::Block;
-use std::path::PathBuf;
+use std::error::Error;
+use storage::Storage;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
 
-// TODO: Move to config file
-const DATA_DIR: &str = "./data";
-
-struct DataNode {
-    /// ID of datanode stored in namenode
-    id: usize,
-
-    /// Blocks stored in datanode
-    blocks: Vec<Block>,
-
-    /// Directory where blocks are stored
-    data_dir: PathBuf,
-
-    /// Port to listen on
-    port: u16,
+pub struct DataNodeServer {
+    server_addr: String,
+    storage: Storage,
 }
 
-impl DataNode {
-    fn new(id: usize) -> Self {
-        let data_dir = PathBuf::from(DATA_DIR);
-        DataNode {
-            id,
-            data_dir,
-            blocks: vec![],
-            port: 8000,
+impl DataNodeServer {
+    pub fn new(port: String) -> Self {
+        let server_addr = format!("127.0.0.1:{port}");
+        DataNodeServer {
+            server_addr,
+            storage: Storage::new(),
         }
     }
 
-    fn add_block(id: usize, data: Option<Vec<u8>>) -> Block {
-        let data = match data {
-            Some(data) => data,
-            None => vec![],
-        };
-        Block::new(id, data)
+    pub async fn create_listener(&self) -> Result<TcpListener, Box<dyn Error>> {
+        let listener = TcpListener::bind(&self.server_addr).await?;
+        Ok(listener)
     }
 
-    fn read_block(&self, id: usize) -> Option<&Block> {
-        self.blocks.get(id)
+    pub async fn bind_port(&self) -> Result<TcpStream, Box<dyn Error>> {
+        let mut stream = TcpStream::connect(&self.server_addr).await?;
+        stream.write_all(b"hello world").await?;
+        Ok(stream)
+    }
+}
+
+pub mod storage {
+    use crate::block::Block;
+    use std::path::PathBuf;
+
+    // TODO: Move to config file
+    const DATA_DIR: &str = "./data";
+
+    pub struct Storage {
+        /// Blocks stored in datanode
+        pub blocks: Vec<Block>,
+
+        /// Directory where blocks are stored
+        data_dir: PathBuf,
+    }
+
+    impl Default for Storage {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl Storage {
+        pub fn new() -> Self {
+            let data_dir = PathBuf::from(DATA_DIR);
+            Storage {
+                data_dir,
+                blocks: vec![],
+            }
+        }
+
+        pub fn add_block(&mut self, data: Option<Vec<u8>>) -> Block {
+            let data = match data {
+                Some(data) => data,
+                None => vec![],
+            };
+            let block = Block::new(self.blocks.len(), data);
+            self.blocks.push(block.clone());
+            block
+        }
+
+        pub fn read_block(&self, id: usize) -> Option<&Block> {
+            match self.blocks.get(id) {
+                Some(block) => Some(block),
+                None => None,
+            }
+        }
     }
 }
